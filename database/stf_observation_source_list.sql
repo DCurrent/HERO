a6c293b97a776c68b5e25890c6942f9618316844
Damon V. Caskey <dc@caskeys.com>
2017-05-22 10:08:27 -0400

    Add observation list SP.

---
+++ database/stf_observation_source_list.sql
@@ -0,0 +1,61 @@
+USE [ehsinfo]
+GO
+
+/****** Object:  StoredProcedure [dbo].[stf_observation_source_list]    Script Date: 5/22/2017 9:32:14 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[stf_observation_source_list]
+	
+	-- Parameters
+	
+	-- paging
+	@param_page_current			int				= 1,
+	@param_page_rows			int				= 10,	
+
+	-- Sorting
+	@param_sort_field			tinyint			= 1,
+	@param_sort_order			bit				= 0,
+
+	-- filters
+	@param_date_start			datetime2		= NULL,
+	@param_date_end				datetime2		= NULL
+	
+AS	
+	SET NOCOUNT ON;
+		
+	-- Populate main table var. This is the primary query. Order
+	-- and query details go here.
+			SELECT ROW_NUMBER() OVER(ORDER BY 
+								-- Sort order options here. CASE lists are ugly, but we'd like to avoid
+								-- dynamic SQL for maintainability.								
+								CASE WHEN @param_sort_field = 1 AND @param_sort_order = 0	THEN _main.label			END ASC,
+								CASE WHEN @param_sort_field = 1 AND @param_sort_order = 1	THEN _main.label			END DESC,
+								CASE WHEN @param_sort_field = 2 AND @param_sort_order = 0	THEN _master.create_time	END ASC,
+								CASE WHEN @param_sort_field = 2 AND @param_sort_order = 1	THEN _master.create_time	END DESC) AS id_row_local,
+				_master.id, 
+				_master.id_key,
+				_main.label,
+				_main.details,
+				_main.observation,
+				_main.solution,
+				_master.create_time
+			INTO #cache_primary
+			FROM dbo.tbl_stf_observation_source _main			
+				JOIN tbl_stf_master _master ON _main.id_key = _master.id_key 
+			WHERE _master.active = 1 AND ((_master.create_time BETWEEN @param_date_start AND @param_date_end) OR @param_date_start IS NULL OR @param_date_end IS NULL)
+
+	-- Execute paging SP to output paged records and control data.
+		EXEC master_paging
+				@param_page_current,
+				@param_page_rows
+	
+	
+
+GO
+
+
